ae51d355f7ee5dafc6fcd6444a1f9c84bec78085
Brooks <47866488+BrooksPriv@users.noreply.github.com>
2026-03-19 14:49:48 -0500

    Refactor Wordle function to include word validation
    Updated Wordle function to accept a list of valid words and check input against it. Enhanced duplicate handling and word selection from a random five-letter word set.

--- Wordle.py
+++ Wordle.py
@@ -1,31 +1,35 @@
 from colorama import init, Fore, Style
 from collections import Counter
+from english_words import get_english_words_set
+import random
 
-
-def Wordle(Word):
+def Wordle(Word, l):
     init()
     WordA = list(Word.lower())
-    
 
-    for z in range(1,5):
+
+    for z in range(1,6):
         #Var
         Inp = ""
         z = ""
         t = []
-        
-        
+
+        print(l)
 
         #Ask the player
         while(len(Inp) != 5):
             Inp = input(f"{Fore.BLACK}Enter a 5 letter word: ")
-            if len(Inp) !=5:
+            if len(Inp) !=5 and Inp not in l:
                 print("Try Again (Hint has to be 5 letters)")
-        
+
         #Duplicates
         counts = Counter(Inp)
+        countsWord = Counter(Word)
         duplicates = [char for char, count in counts.items() if count > 1]
-        if duplicates == ([char for char, count in (Counter(WordA)).items() if count >1]):
-            print("test")
+        for x in WordA:
+           # print(x)
+            if x in countsWord == 1:
+                del countsWord[x]
 
         #Makes Input of player into a list
         Inp = list(Inp.lower())
@@ -34,10 +38,9 @@ def Wordle(Word):
         if Inp == WordA:
             z = "".join(Inp)
             return (f"{Fore.GREEN} {z}!!").replace(" ", "")
-        
+
         #Check for right Place
         for x in range(len(WordA)):
-            #t.pop(int(x-1))
             if WordA[x] == Inp[x]:
                 z = (f"{z}{Fore.GREEN} {Word[x]}")
             elif Inp[x] in WordA:
@@ -50,6 +53,10 @@ def Wordle(Word):
             elif WordA[x] != Inp[x]:
                 z = f"{z}{Fore.BLACK}{Inp[x]}"
         print(z.replace(" ", ""))
+    print(f"{Fore.GREEN} {Word}")
 
+word_set = get_english_words_set(['web2'], lower=True)
+five_letter_words = [word for word in word_set if len(word) == 5]
+random_word = random.choice(five_letter_words)
 
-print(Wordle("hello"))
+print(Wordle(random_word,word_set))
